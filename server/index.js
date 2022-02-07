7a8bfde07f17412ee3a53e4369619ff243e353d3
rsquinlan <ryansquinlan01@gmail.com>
2022-02-06 21:13:04 -0700

    cors bs part 2

--- server/index.js
+++ server/index.js
@@ -11,14 +11,13 @@ const dbConfig = require("./config/db-config")
 //allows api calls to other servers
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://lunch-tag-client.herokuapp.com/");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-  });
+});
 
 //allows us to get data from POST request
 app.use(bodyParser.json())
